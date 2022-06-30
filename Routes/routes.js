6863a8650f71b9617b00e259d1873b3c1c17fc19
AngelaBewick <angelagracebewick@gmail.com>
2022-06-30 13:34:43 -0500

    base service and routes

---
+++ Routes/routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+//import functions from repoService
+const { getAllItems, addItem, deleteItem } = require("../repoService.js");
+//use callbacks for specific routes
+router.route("/").get(getAllItems).post(addItem);
+router.route("/:itemID").delete(deleteItem);
+//add ability to update a todo item
+//.get(getTask).patch(updateTask)
+module.exports = router;
--- controller.js
+++ controller.js
@@ -1,12 +1,26 @@
 const express = require("express");
-
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello World");
-});
+const configDB = require("./Configure/configDB.js");
+const routes = require("./Routes/routes.js");
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
+// app.use((req, res) => {
+//   res.send("Hello World");
+// });
+app.use(express.json());
+app.use("/api/v1", routes);
+
+const port = process.env.PORT || 3000;
+
+const start = async function () {
+  try {
+    await mysql.createConnection(config.db);
+
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
