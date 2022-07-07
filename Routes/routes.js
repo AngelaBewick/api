53cf9efa9b63fc6cb89ca3f657de3daf018a17a1
AngelaBewick <angelagracebewick@gmail.com>
2022-07-07 10:17:23 -0500

    getAllItems take 2

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 // //import functions from repoService
 // const { getAllItems, addItem, deleteItem } = require("../repoService.js");
-const getAllItems = require("../repoService.js");
+const { getAllItems } = require("../repoService.js");
+console.log(getAllItems);
 
 // //use callbacks for specific routes
-router.route("/").get(getAllItems).post(addItem);
+router.route("/").get(getAllItems);
+// .post(addItem);
 // router.route("/:itemID").delete(deleteItem);
 // //add ability to update a todo item
 // //.get(getTask).patch(updateTask)
